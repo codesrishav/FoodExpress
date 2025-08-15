9ce774f92e1e5c8e10a2d9ee7a4037eb154fda97
RISHAV KUMAR <152815977+codesrishav@users.noreply.github.com>
2025-08-15 19:52:31 +0530

    Update api.js

--- Fooddelivery/client/src/services/api.js
+++ Fooddelivery/client/src/services/api.js
@@ -1,10 +1,11 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api', // Match this with backend prefix
+  baseURL: 'https://foodexpress-server.onrender.com/api', // Match this with backend prefix
   headers: {
     'Content-Type': 'application/json',
   },
 });
 
 export default API;
+
